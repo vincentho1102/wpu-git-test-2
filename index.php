e501a0293075d590eed2c881a8ffdcb25f61bab3
Vincent Ho <vincentho1102@gmail.com>
2021-01-27 11:06:05 +0700

    Menambah list index.php

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi vel laudantium sed quae architecto exercitationem, mollitia ad fugit atque qui.</p>
+    <ul>
+        <li>Satu</li>
+        <li>Dua</li>
+    </ul>
 </body>
 </html>
